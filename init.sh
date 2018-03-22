7d5f9ccef94a79cea383aa080ea6b06daefdb42a
Radoslaw Mejer <radmen@desmart.com>
2018-03-22 13:51:19 +0100

    Fix errors reported by shellcheck

--- init.sh
+++ init.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # vim: set ft=sh
 
+declare DIR
+
 _docoseal_complete () {
   local cur=${COMP_WORDS[COMP_CWORD]}
   local available_commands="env exec logs purge reset run start stop"
 
-  COMPREPLY=( $(compgen -W "$available_commands" -- $cur) )
+  mapfile -t COMPREPLY < <(compgen -W "$available_commands" -- "$cur")
 }
 
 complete -F _docoseal_complete docoseal
 
-declare -r DIR="$(dirname ${BASH_SOURCE[0]})"
+DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 if test -d "${DIR}/bin"; then
   export PATH="${PATH}:${DIR}/bin"
